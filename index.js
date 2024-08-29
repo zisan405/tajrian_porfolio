8198a2f21dbe0e1fb65a83bab2f70a019883cde5
Zahidul Islam Zisan <zisan7096@gmail.com>
2024-08-29 07:18:11 +0600

    portfolio
    true

---
+++ index.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+
+}
+const typed = new Typed('.multiple-text', {
+    strings: ['Fronted Developer.', 'Backend Developer', 'Blockchain Developer', 'Web Designer'],
+    typeSpeed: 80,
+    backSpeed:  80,
+    backDelay: 1200,
+    loop: true,
+});
